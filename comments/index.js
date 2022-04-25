b8eac88357516350a7679750238839bb666ce7e5
muhseenat <mucktd777@gmail.com>
2022-04-25 23:04:58 +0530

    Comment moderated added in comments service

--- comments/index.js
+++ comments/index.js
@@ -40,9 +40,28 @@ app.post('/posts/:id/comments',async(req,res)=>{
     res.status(201).send(comments)
 })
 
-app.post('/events',(req,res)=>{
+app.post('/events',async(req,res)=>{
     console.log("event ethiyach",req.body.type);
+    const{type,data}=req.body;
+    if(type==="CommentModerated"){
+        const {id,postId,content,status} =data;
+        const comments=commentsByPostId[postId];
+        const comment = comments.find(comment=>{
+            return comment.id== id
+        })
 
+        comment.status=status
+
+        await axios.post('http://localhost:4005/events',{
+            type:"CommentUpdated",
+            data:{
+                id,
+                postId,
+                status,
+                content
+            }
+        })
+    }
     res.send({})
 })
 
