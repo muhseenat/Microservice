b625799fdf602a215e87d100901538de09d8d570
muhseenat <mucktd777@gmail.com>
2022-04-26 00:08:48 +0530

    events Db as well as sync communication added

--- event-bus/index.js
+++ event-bus/index.js
@@ -7,17 +7,32 @@ const app = express();
 
 app.use(bodyParser.json())
 
-app.post('/events',(req,res)=>{
-    const event = req.body;
-    console.log('somethng is coming');
-    axios.post('http://localhost:4000/events',event)
-    axios.post('http://localhost:4001/events',event)
-    axios.post('http://localhost:4002/events',event)
-    axios.post('http://localhost:4003/events',event)
+const events = [];
 
-    res.send({status:"OK"});
+app.post("/events", (req, res) => {
+  const event = req.body;
 
-})
-app.listen(4005,()=>{
-    console.log("Listen to 4005");
-})
+  events.push(event);
+
+  axios.post("http://localhost:4000/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post("http://localhost:4001/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post("http://localhost:4002/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post("http://localhost:4003/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  res.send({ status: "OK" });
+});
+
+app.get("/events", (req, res) => {
+  res.send(events);
+});
+
+app.listen(4005, () => {
+  console.log("Listening on 4005");
+});
--- query/index.js
+++ query/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
+const axios = require ('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -9,13 +9,7 @@ app.use(cors());
 
 const posts = {};
 
-app.get('/posts', (req, res) => {
-    res.send({ posts })
-})
-
-app.post('/events', (req, res) => {
-    const { type, data } = req.body;
-
+const handleEvent=(type,data)=>{
     if (type == 'PostCreated') {
         const { id, title } = data;
         posts[id] = { id, title, comments: [] }
@@ -25,11 +19,43 @@ app.post('/events', (req, res) => {
         const post = posts[postId];
         post.comments.push({ id, content, status });
     }
+    if (type === "CommentUpdated") {
+        const { id, status, content, postId } = data;
+        const post = posts[postId];
+        const comment = post.comments.find(comment => {
+            return comment.id === id;
+        })
+        comment.status = status;
+        comment.content = content;
 
-    console.log(posts);
+    }
+}
+
+app.get('/posts', (req, res) => {
+    res.send({ posts })
+})
+//happend when calling ie on working time
+app.post('/events', (req, res) => {
+    const { type, data } = req.body;
+
+    handleEvent(type,data)
     res.send({})
 })
 
-app.listen(4002, () => {
+app.listen(4002, async() => {
     console.log("Listen to 4002");
+
+    //For proper working if it is down.It takes all events from event db.
+    console.log("Listening on 4002");
+    try {
+      const res = await axios.get("http://localhost:4005/events");
+  
+      for (let event of res.data) {
+        console.log("Processing event:", event.type);
+  
+        handleEvent(event.type, event.data);
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
 })
